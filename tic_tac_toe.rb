8cd809054ddebe27a7c3395554e5d9891dfec714
spaasie <68701772+spaasie@users.noreply.github.com>
2022-06-01 15:49:34 +0200

    add comments to visualise the cli game field

--- tic_tac_toe.rb
+++ tic_tac_toe.rb
@@ -2,6 +2,24 @@
   make 3 colomns namely 1, 2, 3
   make 3 rows namely a, b, c
 
+  start:
+    1 2 3
+  a _|_|_
+  b _|_|_
+  c _|_|_
+
+  rounds 2
+    1 2 3
+  a o|_|_
+  b _|x|_
+  c _|_|_
+
+  late game
+    1 2 3
+  a o|o|x
+  b _|x|o
+  c _|x|o
+  
   make player 1
   make player 2
 =end
