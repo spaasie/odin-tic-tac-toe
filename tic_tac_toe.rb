c5953e2b5a1a09abea759b6b4fc0880d52a3ba51
spaasie <68701772+spaasie@users.noreply.github.com>
2022-06-02 11:25:45 +0200

    add pseudo code for basic build with winning conditions

--- tic_tac_toe.rb
+++ tic_tac_toe.rb
@@ -19,13 +19,13 @@
   a o|o|x
   b _|x|o
   c _|x|o
-  
+
   make player 1
   make player 2
 =end
 
+# Create and manage Player state
 class Player
-
   @moves = []
   def initialize(name)
     @name = name
@@ -34,6 +34,32 @@ class Player
   def choice(entry)
     @moves.push(entry)
   end
-
 end
-    
+
+# pseudo code
+# ask player 1 for name
+# ask player 2 for name
+# draw grid with co-ords for player to choose from
+# ask #{player1.name} for co-ords
+# check if co-ords are available and if (winning = true)
+# draw new grid with player 1 choice
+# ask #{player2.name} for co-ords
+# check if co-ords are available and if (winning = true)
+
+# winning conditions
+#   1 2 3
+# a _|_|_
+# b _|_|_
+# c  | |
+
+# ['a1', 'a2', 'a3']
+# ['b1', 'b2', 'b3']
+# ['c1', 'c2', 'c3']
+#
+# ['a1', 'b1', 'c1']
+# ['a2', 'b2', 'c2']
+# ['a3', 'b3', 'c3']
+#
+# ['a1', 'b2', 'c3']
+# ['c1', 'b2', 'a3']
+#
