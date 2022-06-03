95b4aac65d27447c3b0488100f663376c5bde701
spaasie <68701772+spaasie@users.noreply.github.com>
2022-06-03 09:02:49 +0200

    pre branch commit

--- tic_tac_toe.rb
+++ tic_tac_toe.rb
@@ -63,3 +63,97 @@ end
 # ['a1', 'b2', 'c3']
 # ['c1', 'b2', 'a3']
 #
+puts 'Please enter a name for player 1'
+player_1_name = gets.chomp
+puts "Thank you #{player_1_name}"
+
+puts 'Please enter a name for player 2'
+player_2_name = gets.chomp
+puts "Thank you #{player_2_name}"
+
+puts 'Lets start shall we?'
+
+puts ' 1 2 3'
+puts 'a_|_|_'
+puts 'b_|_|_'
+puts 'c | |'
+
+puts "#{player_1_name} it is your turn please enter a co-ord"
+player_1_choice = gets.chomp
+
+player_1_marks = []
+player_2_marks = []
+
+player_1_marks.push(player_1_choice)
+
+# print board with player 1 marks
+
+# Board with co-ordinates for players to play on
+class Board
+  attr_reader :board
+
+  @a1 = '_'
+  @a2 = '_'
+  @a3 = '_'
+  @b1 = '_'
+  @b2 = '_'
+  @b3 = '_'
+  @c1 = ' '
+  @c2 = ' '
+  @c3 = ' '
+
+  @row_a = [
+    'a ',
+    @a1,
+    '|',
+    @a2,
+    '|',
+    @a3
+  ]
+
+  @row_b = [
+    'b ',
+    @b1,
+    '|',
+    @b2,
+    '|',
+    @b3
+  ]
+
+  @row_c = [
+    'c ',
+    @c1,
+    '|',
+    @c2,
+    '|',
+    @c3
+  ]
+
+  @board = [
+    '  1 2 3',
+    @row_a,
+    @row_b,
+    @row_c
+  ]
+
+  # def initialize
+  #   [@a1, @a2, @a3, @b1, @b2, @b3].map { |variable| variable = '_' }
+  #   [@c1, @c2, @c3].map { |variable| variable = ' ' }
+  # end
+
+  def call
+    puts "here is : #{@a1}"
+  end
+
+  def show_board
+    @board.each do |row|
+      puts row
+    end
+  end
+end
+
+game = Board.new
+p game.board
+game.call
+
+game.show_board
