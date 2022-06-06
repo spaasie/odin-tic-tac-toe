559b2cfa5b400154e121d3958fa37e1bb00f75a1
spaasie <68701772+spaasie@users.noreply.github.com>
2022-06-06 16:32:31 +0200

    create Player class and manage user choices

--- tic_tac_toe.rb
+++ tic_tac_toe.rb
@@ -24,136 +24,99 @@
   make player 2
 =end
 
-# Create and manage Player state
+# Basic mold for a player
 class Player
-  @moves = []
+  attr_reader :name, :player, :marks
+
+  @@player_num = 0
+  @@win_combo = [
+    %w[a1 a2 a3],
+    %w[b1 b2 b3],
+    %w[c1 c2 c3],
+    %w[a1 b2 c3],
+    %w[a3 b2 c1],
+    %w[a1 b1 c1],
+    %w[a2 b2 c2],
+    %w[a3 b3 c3]
+  ]
+  @@all_marks = []
   def initialize(name)
+    @@player_num += 1
+    @player = @@player_num
     @name = name
+    @marks = []
   end
 
-  def choice(entry)
-    @moves.push(entry)
-  end
-end
-
-# pseudo code
-# ask player 1 for name
-# ask player 2 for name
-# draw grid with co-ords for player to choose from
-# ask #{player1.name} for co-ords
-# check if co-ords are available and if (winning = true)
-# draw new grid with player 1 choice
-# ask #{player2.name} for co-ords
-# check if co-ords are available and if (winning = true)
-
-# winning conditions
-#   1 2 3
-# a _|_|_
-# b _|_|_
-# c  | |
-
-# ['a1', 'a2', 'a3']
-# ['b1', 'b2', 'b3']
-# ['c1', 'c2', 'c3']
-#
-# ['a1', 'b1', 'c1']
-# ['a2', 'b2', 'c2']
-# ['a3', 'b3', 'c3']
-#
-# ['a1', 'b2', 'c3']
-# ['c1', 'b2', 'a3']
-#
-puts 'Please enter a name for player 1'
-player_1_name = gets.chomp
-puts "Thank you #{player_1_name}"
-
-puts 'Please enter a name for player 2'
-player_2_name = gets.chomp
-puts "Thank you #{player_2_name}"
-
-puts 'Lets start shall we?'
-
-puts ' 1 2 3'
-puts 'a_|_|_'
-puts 'b_|_|_'
-puts 'c | |'
-
-puts "#{player_1_name} it is your turn please enter a co-ord"
-player_1_choice = gets.chomp
-
-player_1_marks = []
-player_2_marks = []
-
-player_1_marks.push(player_1_choice)
-
-# print board with player 1 marks
-
-# Board with co-ordinates for players to play on
-class Board
-  attr_reader :board
-
-  @a1 = '_'
-  @a2 = '_'
-  @a3 = '_'
-  @b1 = '_'
-  @b2 = '_'
-  @b3 = '_'
-  @c1 = ' '
-  @c2 = ' '
-  @c3 = ' '
-
-  @row_a = [
-    'a ',
-    @a1,
-    '|',
-    @a2,
-    '|',
-    @a3
-  ]
-
-  @row_b = [
-    'b ',
-    @b1,
-    '|',
-    @b2,
-    '|',
-    @b3
-  ]
-
-  @row_c = [
-    'c ',
-    @c1,
-    '|',
-    @c2,
-    '|',
-    @c3
-  ]
-
-  @board = [
-    '  1 2 3',
-    @row_a,
-    @row_b,
-    @row_c
-  ]
-
-  # def initialize
-  #   [@a1, @a2, @a3, @b1, @b2, @b3].map { |variable| variable = '_' }
-  #   [@c1, @c2, @c3].map { |variable| variable = ' ' }
-  # end
-
-  def call
-    puts "here is : #{@a1}"
+  def winning?
+    @@win_combo.any? do |arr|
+      arr.all? do |i|
+        marks.include?(i)
+      end
+    end
   end
 
-  def show_board
-    @board.each do |row|
-      puts row
+  def co_ord(co_ord)
+    if !@@all_marks.include?(co_ord)
+      @@all_marks.push(co_ord)
+      @marks.push(co_ord)
+    else
+      puts "#{co_ord} has already been chosen"
     end
   end
 end
+# pseudo code
+# ask player 1 for name
 
-game = Board.new
-p game.board
-game.call
+puts 'Please enter a name for player 1'
+# player_1_name = gets.chomp
+player_1_name = gets.chomp
+player1 = Player.new(player_1_name)
+puts "Thank you #{player1.name}"
 
-game.show_board
+# ask player 2 for name
+puts 'Please enter a name for player 2'
+player_2_name = gets.chomp
+player2 = Player.new(player_2_name)
+puts "Thank you #{player2.name}"
+
+# draw grid with co-ords for player to choose from
+puts 'Lets start shall we?'
+puts 'Please select a location to make your mark e.g. a1 or b2'
+puts '  1 2 3'
+puts 'a _|_|_'
+puts 'b _|_|_'
+puts 'c  | |'
+
+# ask #{player1.name} for co-ords
+puts "#{player_1_name} it is your turn please enter a co-ord"
+player_1_choice = gets.chomp
+# player_1_marks = []
+# player_1_marks.push(player_1_choice)
+player1.co_ord(player_1_choice)
+puts player1.player
+puts player1.marks
+
+# check if co-ords are available and if (winning = true)
+# win_combo = [
+#   %w[a1 a2 a3],
+#   %w[b1 b2 b3],
+#   %w[c1 c2 c3],
+#   %w[a1 b2 c3],
+#   %w[a3 b2 c1],
+#   %w[a1 b1 c1],
+#   %w[a2 b2 c2],
+#   %w[a3 b3 c3]
+# ]
+
+
+# puts winning?(player_1_marks)
+# draw new grid with player 1 choice
+# ask #{player2.name} for co-ords
+puts "#{player_2_name} it is your turn, please enter a co-ord"
+player_2_choice = gets.chomp
+# player_2_marks = []
+player2.co_ord(player_2_choice)
+# check if co-ords are available and if (winning = true)
+# print board with player 1 marks
+
+# Board with co-ordinates for players to play on
