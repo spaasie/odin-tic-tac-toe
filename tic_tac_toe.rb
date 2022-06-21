197852bbf902d8b468877d78e7bbc8993560fba8
spaasie <68701772+spaasie@users.noreply.github.com>
2022-06-21 09:42:10 +0200

    move noughts and crosses to tic tac toe file

--- tic_tac_toe.rb
+++ tic_tac_toe.rb
@@ -1,105 +1,122 @@
-=begin
-  make 3 colomns namely 1, 2, 3
-  make 3 rows namely a, b, c
+# frozen_string_literal: true
 
-  start:
-    1 2 3
-  a _|_|_
-  b _|_|_
-  c _|_|_
+# Create Game instance to keep track of winning state
+class Game
+  attr_reader :board
 
-  rounds 2
-    1 2 3
-  a o|_|_
-  b _|x|_
-  c _|_|_
-
-  late game
-    1 2 3
-  a o|o|x
-  b _|x|o
-  c _|x|o
-
-  make player 1
-  make player 2
-=end
-
-# Basic mold for a player
-class Player
-  attr_reader :name, :player, :marks
-
-  @@player_num = 0
-  @@win_combo = [
+  WIN_COMBO = [
     %w[a1 a2 a3],
     %w[b1 b2 b3],
     %w[c1 c2 c3],
-    %w[a1 b2 c3],
-    %w[a3 b2 c1],
     %w[a1 b1 c1],
     %w[a2 b2 c2],
-    %w[a3 b3 c3]
-  ]
-  @@all_marks = []
-  def initialize(name)
-    @@player_num += 1
-    @player = @@player_num
-    @name = name
-    @marks = %w[a1 a2]
+    %w[a3 b3 c3],
+    %w[a1 b2 c3],
+    %w[a3 b2 c1]
+  ].freeze
+
+  def initialize
+    @total_choices = []
+    @board_hash = {
+      a1: '_',
+      a2: '_',
+      a3: '_',
+      b1: '_',
+      b2: '_',
+      b3: '_',
+      c1: ' ',
+      c2: ' ',
+      c3: ' '
+    }
+    @board = [
+      '  1 2 3',
+      "a #{@board_hash[:a1]}|#{@board_hash[:a2]}|#{@board_hash[:a3]}",
+      "b #{@board_hash[:b1]}|#{@board_hash[:b2]}|#{@board_hash[:b3]}",
+      "c #{@board_hash[:c1]}|#{@board_hash[:c2]}|#{@board_hash[:c3]}"
+    ]
   end
 
-  def winning?
-    @@win_combo.any? do |arr|
-      arr.all? do |i|
-        marks.include?(i)
+  def self.winning?(player_object)
+    self::WIN_COMBO.any? do |combo|
+      combo.all? do |i|
+        player_object.choices.include?(i)
       end
     end
   end
 
-  def co_ord(co_ord)
-    if !@@all_marks.include?(co_ord)
-      @@all_marks.push(co_ord)
-      @marks.push(co_ord)
-    else
-      puts "#{co_ord} has already been chosen"
-    end
+  def available?(player_choice)
+    !@total_choices.include?(player_choice)
+  end
+
+  def choice(string)
+    @total_choices.push(string)
+  end
+
+  def update_board(player1_choices, player2_choices)
+    player1_choices.map { |i| @board_hash[i.to_sym] = 'x' }
+    player2_choices.map { |i| @board_hash[i.to_sym] = 'o' }
+    @board = [
+      '  1 2 3',
+      "a #{@board_hash[:a1]}|#{@board_hash[:a2]}|#{@board_hash[:a3]}",
+      "b #{@board_hash[:b1]}|#{@board_hash[:b2]}|#{@board_hash[:b3]}",
+      "c #{@board_hash[:c1]}|#{@board_hash[:c2]}|#{@board_hash[:c3]}"
+    ]
   end
 end
-# pseudo code
-# ask player 1 for name
 
-puts 'Please enter a name for player 1'
-# player_1_name = gets.chomp
-player_1_name = gets.chomp
-player1 = Player.new(player_1_name)
-puts "Thank you #{player1.name}"
+# Create a Player instance keeping track of choises and state
+class Player
+  attr_reader :name, :choices
 
-# ask player 2 for name
-puts 'Please enter a name for player 2'
-player_2_name = gets.chomp
-player2 = Player.new(player_2_name)
-puts "Thank you #{player2.name}"
+  def initialize(name)
+    @name = name
+    @choices = []
+  end
 
-# draw grid with co-ords for player to choose from
-puts 'Lets start shall we?'
-puts 'Please select a location to make your mark e.g. a1 or b2'
-puts '  1 2 3'
-puts 'a _|_|_'
-puts 'b _|_|_'
-puts 'c  | |'
+  def self.valid_choice?(string)
+    # check if valid choice [a-c][1-3] regex
+    string.match?(/[a-c][1-3]/)
+  end
 
-def play(player_object)
-  puts "#{player_object.name} it is your turn please enter a co-ord"
-  player_object.co_ord(gets.chomp)
-  player_object.winning?
+  def choice(string)
+    @choices.push(string)
+  end
 end
-# puts "yeay #{player1.name} won" if play(player1)
-# puts "yeay #{player2.name} won" if play(player2)
 
+def clear_lines(number)
+  print "\r#{"\e[A\e[K" * number}"
+end
+
+# Initialising Player and game
+puts 'what is your name player 1?'
+player1 = Player.new(gets.chomp)
+clear_lines(2)
+puts 'what is your name player 2?'
+player2 = Player.new(gets.chomp)
+clear_lines(2)
+current_game = Game.new
+
+players = [player1, player2]
+# Basic loop
 playing = true
 round = 0
 
 while playing
-  players = [player1, player2]
-  playing = false if play(players[round % 2])
-  round += 1
+  current_player = players[round % 2]
+  if playing
+    # ask player for choise
+    puts "#{current_player.name} please choose a location"
+    puts current_game.board
+    player_choice = gets.chomp
+    if Player.valid_choice?(player_choice) && current_game.available?(player_choice)
+      clear_lines(6)
+      current_player.choice(player_choice)
+      current_game.choice(player_choice)
+      round += 1
+      current_game.update_board(player1.choices, player2.choices)
+    else
+      clear_lines(6)
+    end
+    playing = false if Game.winning?(current_player)
+  end
 end
